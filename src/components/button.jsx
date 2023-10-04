ec05c24833d91e9ec98435199d25652a70a0a0d8
Onderito <ulas.onder@outlook.fr>
2023-10-04 16:53:26 +0200

    specialOffer layout finish

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,6 +1,13 @@
-function Button({ label, iconURL }) {
+function Button({ label, iconURL, backgroundColor, borderColor, textColor }) {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center rounded-full items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red rounded-full text-white border-coral-red"
+      }`}
+    >
       {label}
       {iconURL && (
         <img
